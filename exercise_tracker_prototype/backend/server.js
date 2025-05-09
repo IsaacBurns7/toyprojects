e56364a18fb45720f32c6f603e666096eca8cecc
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2025-05-09 12:53:40 -0500

    authenticating

--- exercise_tracker_prototype/backend/server.js
+++ exercise_tracker_prototype/backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // express app
 const app = express();
@@ -10,15 +11,14 @@ const app = express();
 // middleware
 app.use(express.json());
 
-
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 });
 
 // routes
-app.use('/api/workouts', workoutRoutes)
-
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect(process.env.MONGO_ATLAS_URI)
     .then(() => {
