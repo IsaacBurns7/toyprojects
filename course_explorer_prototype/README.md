971e58fce641491610ddf891652a053b569b32f8
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2025-05-18 20:47:37 -0500

    added anexing

---
+++ course_explorer_prototype/README.md
@@ -0,0 +1,1 @@
+This project is intended to make it easier for A&M Students to figure out what classes are right for them.
--- course_explorer_prototype/backend/index.js
+++ course_explorer_prototype/backend/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 
-(async () => {
+const createHtml = async () => {
     const response = await fetch("https://catalog.tamu.edu/undergraduate/course-descriptions/csce/", {
         method: "GET"
     })
@@ -33,4 +33,52 @@ const cheerio = require('cheerio');
 
     fs.writeFileSync('index.html', wrappedHtml);
 
-})();
+};
+
+//
+const attachProfessors = async () => {
+    const html = fs.readFileSync("index.html", "utf-8");
+    const $ = cheerio.load(html);
+    const elements = $('.courseblock').toArray();
+
+    for(const element of elements){
+        const desc = $(element).find('.courseblockdesc');
+        desc.after('<button>Click me to show professors</button>');
+
+        const title = $(element).find('.courseblocktitle');
+        const dept = title.text().slice(0,4);
+        const number = title.text().slice(5,8);
+        console.log(dept, number);
+        
+        try{
+            const anex_data = await getAnexData(dept, number);
+            fs.writeFileSync(`${dept}_${number}.json`, JSON.stringify(anex_data, null, 2));
+            //<add button and professor list here>
+        }catch(error){
+            console.error(`Failed for ${dept} ${number}: `, error);
+            //note: if this fails, that does mean that there is NO data on this class. Probably is not taught anymore.
+        }
+
+        //desc.after(${professor stuff});
+    };
+}
+
+const getAnexData = async (dept, number) => {
+    const params = new URLSearchParams();
+    params.append('dept', dept);
+    params.append('number', number);
+    const response = await fetch('https://anex.us/grades/getData/?', {
+        method: 'POST',
+        body: params,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0 Gecko/20100101 Firefox/128.0"
+        }
+    });
+    const anex_data = await response.json();
+    return anex_data;
+    //fs.writeFileSync('anex.json', JSON.stringify(anex_data, null, 2));
+}
+
+createHtml();
+getAnexData("CSCE", "110");
+attachProfessors();
